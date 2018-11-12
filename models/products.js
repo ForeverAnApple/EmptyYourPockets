69be6a673b38a0b60b01487534dba6a392661df2
Gangadhar Adusumalli <gangadharadusumalli1993@gmail.com>
2018-11-11 21:04:15 -0600

    product model and data folder changes

---
+++ models/products.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const ProductSchema = new mongoose.Schema({
+
+  _id: { type: Number, required: true },
+  productName: {
+    type: String,
+    required: true,
+    default: 'Iphone'
+  }, 
+  productDescription: {
+    type: String,
+    required: true,
+    default: 'Mobile'
+  }, 
+  price: {
+    type: Number,
+    required: false,
+    default:600
+  },
+  productCategory: { 
+    type: String,
+    required: true,
+    default: 'Electronic'
+  },
+  sellerId: { 
+    type: Number,
+    required: true,
+  }
+})
+module.exports = mongoose.model('Product', ProductSchema)
